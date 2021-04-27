22bace017174bfc7bd7a61b9541bf78d02aaf2c8
Joseph Soukup <joseph.soukup@outlook.com>
2021-04-27 19:34:57 +0200

    router, home

---
+++ src/containers/faqs.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Accordion } from "../components";
+import OptForm from "../components/opt-form";
+import fasqData from "../fixtures/faqs.json";
+
+export function FaqsContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      {fasqData.map((item) => {
+        return (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        );
+      })}
+      <Accordion.Item />
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership
+        </OptForm.Text>
+      </OptForm>
+    </Accordion>
+  );
+}
