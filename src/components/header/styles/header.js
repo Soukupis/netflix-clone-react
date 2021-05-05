03f228ba470488e2f04e145f77236af6d69956bc
Joseph Soukup <joseph.soukup@outlook.com>
2021-05-05 22:00:04 +0200

    footer, slider with cards finished

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -143,6 +143,7 @@ export const PlayButton = styled.button`
   font-size: 20px;
   margin-top: 30px;
   curson-pointer;
+  transition: background-color 0.2s;
 
   &:hover{
     background-color: #ff1e1e;
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
+import { FooterContainer } from "./footer";
 import { FirebaseContext } from "../context/firebase";
 import { Loading, Header, Card } from "../components";
 import * as ROUTES from "../constants/routes";
@@ -79,7 +80,30 @@ export function BrowseContainer({ slides }) {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
-      <Card.Group></Card.Group>
+      <Card.Group>
+        {slideRows.map((slideItem) => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image
+                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                  />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            <Card.Feature category={category}>
+              <p>hej</p>
+            </Card.Feature>
+          </Card>
+        ))}
+      </Card.Group>
+      <FooterContainer />
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
